58b56c6c758627e6aa70c047144f4d819a0174e0
OswaldDB <kevindeanathey@hotmail.com>
2020-10-25 18:03:03 -0500

    giving up for the night

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from scrape_mars import scrape
 import pymongo
 
@@ -6,7 +6,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "This is the index"
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+    db = client.marsDB
+    allData = db.allData.find()
+    for dictionary in allData:
+        final_dict = dictionary
+    headlines_list = final_dict['headlines']
+    paragraphs_list = final_dict['paragraphs']
+    picture = final_dict['picture']
+    mars_data_dict = final_dict['mars_data']
+    hemispheres_dict = final_dict['hemispheres']
+    return render_template("index.html", headlines_list = headlines_list, paragraphs_list = paragraphs_list, picture = picture, mars_data_dict = mars_data_dict, hemispheres_dict = hemispheres_dict)
 
 @app.route("/scrape")
 def port_scrape():
@@ -16,7 +27,7 @@ def port_scrape():
     db = client.marsDB
     allData = db.allData.find()
     db.allData.insert_one(final_dict)
-    return "This is the scrape page"
+    return str(final_dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
